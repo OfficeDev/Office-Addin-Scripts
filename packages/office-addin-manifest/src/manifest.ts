e8b8b67631ef9eff8039421708db46f5b1a0b01b
Adam Krantz <Adam.Krantz@microsoft.com>
2018-09-14 13:16:18 -0700

    output blank string intead of undefined

--- packages/office-addin-manifest/src/manifest.ts
+++ packages/office-addin-manifest/src/manifest.ts
@@ -85,19 +85,19 @@ function parseManifest(xml: any): ManifestInfo {
 
     return manifest;
 }
-  
+
 async function infoCommandAction(path: string) {
     try {
         const manifest = await readManifestFile(path);
 
         console.log(`Manifest: ${path}`);
-        console.log(`  Id: ${manifest.id}`)
-        console.log(`  Name: ${manifest.displayName}`)
-        console.log(`  Provider: ${manifest.providerName}`)
-        console.log(`  Type: ${manifest.officeAppType}`)
-        console.log(`  Version: ${manifest.version || ""}`)
-        console.log(`  Default Locale: ${manifest.defaultLocale}`)
-        console.log(`  Description: ${manifest.description}`)
+        console.log(`  Id: ${manifest.id || ""}`);
+        console.log(`  Name: ${manifest.displayName || ""}`);
+        console.log(`  Provider: ${manifest.providerName || ""}`);
+        console.log(`  Type: ${manifest.officeAppType || ""}`);
+        console.log(`  Version: ${manifest.version || ""}`);
+        console.log(`  Default Locale: ${manifest.defaultLocale || ""}`);
+        console.log(`  Description: ${manifest.description || ""}`);
     } catch (err) {
         console.error(`Error: ${err}`);
     }
