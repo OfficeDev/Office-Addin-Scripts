5e93e819d2b278cd7b500ddfd4c89b264e34ec1b
Adam Krantz <Adam.Krantz@microsoft.com>
2018-09-14 05:34:39 -0700

    read more manifest info

--- packages/office-addin-manifest/src/manifest.ts
+++ packages/office-addin-manifest/src/manifest.ts
@@ -9,7 +9,11 @@ import * as xml2js from 'xml2js';
 
 export interface ManifestInfo {
     id?: string;
-    name?: string;
+    defaultLocale?: string;
+    description?: string;
+    displayName?: string;
+    officeAppType?: string;
+    providerName?: string;
     version?: string;
 }
 
@@ -40,21 +44,49 @@ export function readManifestFile(manifestPath: string): Promise<ManifestInfo> {
     });
 }
 
+function xmlAttributeValue(xml: any, name: string): string {    
+    return xml['$'][name];
+}
+
+function xmlElementAttributeValue(xml: any, elementName: string, attributeName: string = 'DefaultValue'): string {
+    const element = xmlElementValue(xml, elementName);
+    return xmlAttributeValue(element, attributeName);
+}
+
+function xmlElementValue(xml: any, name: string): string {
+    return xml[name][0];
+}
+
 function parseManifest(xml: any): ManifestInfo {    
     const manifest: ManifestInfo = { }; 
     const officeApp = xml['OfficeApp'];
-
-    manifest.id = officeApp['Id'][0];
+    
+    manifest.id = xmlElementValue(officeApp, 'Id');
+    manifest.officeAppType = xmlAttributeValue(officeApp, 'xsi:type');
+    manifest.defaultLocale = xmlElementValue(officeApp, 'DefaultLocale');
+    manifest.description = xmlElementAttributeValue(officeApp, 'Description');
+    manifest.displayName = xmlElementAttributeValue(officeApp, 'DisplayName');
+    manifest.providerName = xmlElementValue(officeApp, 'ProviderName');
+    manifest.version = xmlElementValue(officeApp, 'Version');
 
     return manifest;
 }
   
+async function infoCommandAction(path: string) {
+    const manifest = await readManifestFile(path);
+
+    console.log(`Manifest: ${path}`);
+    console.log(`  Id: ${manifest.id}`)
+    console.log(`  Name: ${manifest.displayName}`)
+    console.log(`  Provider: ${manifest.providerName}`)
+    console.log(`  Type: ${manifest.officeAppType}`)
+    console.log(`  Version: ${manifest.version}`)
+    console.log(`  Default Locale: ${manifest.defaultLocale}`)
+    console.log(`  Description: ${manifest.description}`)
+}
+
 commander
     .command('info [path]')
-    .action(async function (path: string) {
-        const manifest = await readManifestFile(path);
-        console.log(`Manifest: ${path}`);
-        console.log(`  Id: ${manifest.id}`)
-    });
+    .action(infoCommandAction);
 
 commander.parse(process.argv);
