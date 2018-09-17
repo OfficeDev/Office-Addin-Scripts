a51ca328c8a769210964daa8f85ac1d53e0a2f9c
Adam Krantz <Adam.Krantz@microsoft.com>
2018-09-17 13:14:17 -0700

    dev-settings: move redundent code into validateManifestId()

--- packages/office-addin-dev-settings/src/commands.ts
+++ packages/office-addin-dev-settings/src/commands.ts
@@ -2,18 +2,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { readManifestFile } from "office-addin-manifest";
+import { ManifestInfo, readManifestFile } from "office-addin-manifest";
 import * as devSettings from "./dev-settings";
 
 export async function clear(manifestPath: string) {
     try {
         const manifest = await readManifestFile(manifestPath);
 
-        if (manifest.id) {
-            devSettings.clearDevSettings(manifest.id);
-        } else {
-            throw new Error(`The manifest file doesn't contain the id of the Office add-in.`);
-        }
+        validateManifestId(manifest);
+
+        devSettings.clearDevSettings(manifest.id!);
     } catch (err) {
         console.error(`Error: ${err}`);
     }
@@ -23,11 +21,9 @@ export async function disableDebugging(manifestPath: string) {
     try {
         const manifest = await readManifestFile(manifestPath);
 
-        if (manifest.id) {
-            devSettings.disableDebugging(manifest.id);
-        } else {
-            throw new Error(`The manifest file doesn't contain the id of the Office add-in.`);
-        }
+        validateManifestId(manifest);
+
+        devSettings.disableDebugging(manifest.id!);
     } catch (err) {
         console.error(`Error: ${err}`);
     }
@@ -37,12 +33,16 @@ export async function enableDebugging(manifestPath: string) {
     try {
         const manifest = await readManifestFile(manifestPath);
 
-        if (manifest.id) {
-            devSettings.enableDebugging(manifest.id);
-        } else {
-            throw new Error(`The manifest file doesn't contain the id of the Office add-in.`);
-        }
+        validateManifestId(manifest);
+
+        devSettings.enableDebugging(manifest.id!);
     } catch (err) {
         console.error(`Error: ${err}`);
+    }
+}
+
+function validateManifestId(manifest: ManifestInfo) {
+    if (!manifest.id) {
+        throw new Error(`The manifest file doesn't contain the id of the Office add-in.`);
     }
 }
