4fb3eb1f05e5a109c512ad21f21db91e095371de
Adam Krantz <Adam.Krantz@microsoft.com>
2018-09-19 08:59:30 -0700

    manifest: use indent of 2 spaces

--- packages/office-addin-manifest/src/manifest.ts
+++ packages/office-addin-manifest/src/manifest.ts
@@ -9,91 +9,91 @@ import * as xml2js from "xml2js";
 import * as commands from "./commands";
 
 export class ManifestInfo {
-    public id?: string;
-    public defaultLocale?: string;
-    public description?: string;
-    public displayName?: string;
-    public officeAppType?: string;
-    public providerName?: string;
-    public version?: string;
+  public id?: string;
+  public defaultLocale?: string;
+  public description?: string;
+  public displayName?: string;
+  public officeAppType?: string;
+  public providerName?: string;
+  public version?: string;
 }
 
 function parseManifest(xml: any): ManifestInfo {
-    const manifest: ManifestInfo = { };
-    const officeApp = xml.OfficeApp;
+  const manifest: ManifestInfo = { };
+  const officeApp = xml.OfficeApp;
 
-    manifest.id = xmlElementValue(officeApp, "Id");
-    manifest.officeAppType = xmlAttributeValue(officeApp, "xsi:type");
-    manifest.defaultLocale = xmlElementValue(officeApp, "DefaultLocale");
-    manifest.description = xmlElementAttributeValue(officeApp, "Description");
-    manifest.displayName = xmlElementAttributeValue(officeApp, "DisplayName");
-    manifest.providerName = xmlElementValue(officeApp, "ProviderName");
-    manifest.version = xmlElementValue(officeApp, "Version");
+  manifest.id = xmlElementValue(officeApp, "Id");
+  manifest.officeAppType = xmlAttributeValue(officeApp, "xsi:type");
+  manifest.defaultLocale = xmlElementValue(officeApp, "DefaultLocale");
+  manifest.description = xmlElementAttributeValue(officeApp, "Description");
+  manifest.displayName = xmlElementAttributeValue(officeApp, "DisplayName");
+  manifest.providerName = xmlElementValue(officeApp, "ProviderName");
+  manifest.version = xmlElementValue(officeApp, "Version");
 
-    return manifest;
+  return manifest;
 }
 
 export function readManifestFile(manifestPath: string): Promise<ManifestInfo> {
-    return new Promise(async function(resolve, reject) {
-        if (manifestPath) {
-            try {
-            fs.readFile(manifestPath, function(readError, fileData) {
-                    if (readError) {
-                        reject(`Unable to read the manifest file: ${manifestPath}. \n${readError}`);
-                    } else {
-                        // tslint:disable-next-line:only-arrow-functions
-                        xml2js.parseString(fileData, function(parseError, result) {
-                            if (parseError) {
-                                reject(`Unable to parse the manifest file: ${manifestPath}. \n${parseError}`);
-                            } else {
-                                try {
-                                    const manifest: ManifestInfo = parseManifest(result);
-                                    resolve (manifest);
-                                } catch (err) {
-                                    reject(`Unable to parse the manifest file: ${manifestPath}. \n${err}`);
-                                }
-                            }
-                        });
-                    }
-                });
-            } catch (err) {
-            return reject(`Unable to read the manifest file: ${manifestPath}. \n${err}`);
-            }
-        } else {
-            reject(`Please provide the path to the manifest file.`);
-        }
-    });
+  return new Promise(async function(resolve, reject) {
+    if (manifestPath) {
+      try {
+        fs.readFile(manifestPath, function(readError, fileData) {
+          if (readError) {
+            reject(`Unable to read the manifest file: ${manifestPath}. \n${readError}`);
+          } else {
+            // tslint:disable-next-line:only-arrow-functions
+            xml2js.parseString(fileData, function(parseError, result) {
+              if (parseError) {
+                reject(`Unable to parse the manifest file: ${manifestPath}. \n${parseError}`);
+              } else {
+                try {
+                  const manifest: ManifestInfo = parseManifest(result);
+                  resolve (manifest);
+                } catch (err) {
+                  reject(`Unable to parse the manifest file: ${manifestPath}. \n${err}`);
+                }
+              }
+            });
+          }
+        });
+      } catch (err) {
+        return reject(`Unable to read the manifest file: ${manifestPath}. \n${err}`);
+      }
+    } else {
+      reject(`Please provide the path to the manifest file.`);
+    }
+  });
 }
 
 function xmlAttributeValue(xml: any, name: string): string | undefined {
-    try {
-        return xml.$[name];
-    } catch (err) {
-        // console.error(`Unable to get xml attribute value "${name}". ${err}`);
-    }
+  try {
+    return xml.$[name];
+  } catch (err) {
+    // console.error(`Unable to get xml attribute value "${name}". ${err}`);
+  }
 }
 
 function xmlElementAttributeValue(xml: any, elementName: string, attributeName: string = "DefaultValue"): string | undefined {
-    const element = xmlElementValue(xml, elementName);
-    if (element) {
-        return xmlAttributeValue(element, attributeName);
-    }
+  const element = xmlElementValue(xml, elementName);
+  if (element) {
+    return xmlAttributeValue(element, attributeName);
+  }
 }
 
 function xmlElementValue(xml: any, name: string): string | undefined {
-    try {
-        const element = xml[name];
+  try {
+    const element = xml[name];
 
-        if (element) {
-            return element[0];
-        }
-    } catch (err) {
-        // console.error(`Unable to get xml element value "${name}". ${err}`);
+    if (element) {
+      return element[0];
     }
+  } catch (err) {
+    // console.error(`Unable to get xml element value "${name}". ${err}`);
+  }
 }
 
 commander
-    .command("info [path]")
-    .action(commands.info);
+  .command("info [path]")
+  .action(commands.info);
 
 commander.parse(process.argv);
