e52660589be6d7f0eb479cd4227022b66c3c73b0
Don Gunther <donaldgu@microsoft.com>
2018-10-18 11:58:52 -0700

    update to tests

---
+++ packages/custom-functions-metadata/test/errorfunctions.js
@@ -0,0 +1,8 @@
+/**
+ * This function is testing add
+ * @CustomFunction
+ * @param {badtype} num1 - testing add
+ * @return {number} - return number
+ */
+function testadd(num1){
+}
--- packages/custom-functions-metadata/test/test.ts
+++ packages/custom-functions-metadata/test/test.ts
@@ -3,24 +3,10 @@ import * as mocha from "mocha";
 import * as fs from "fs";
 import * as ts from "typescript";
 import * as jsongenerator from "../src/custom-functions-metadata";
-//import * as mini from "minimist";
-
-var path = require("path");
-var argv = require("optimist").demand('config').argv;
-var configFilePath = argv.config;
-
-assert.ok(fs.existsSync(configFilePath), 'config file not found:' + configFilePath);
-var config = require('nconf').env().argv().file({file: configFilePath});
-
-var goodString:string = "functions.json created for file: src/testfunctions.ts";
-
-var pathKey = config.get('jsonfile');
-var pathlocation = pathKey.path;
 
 describe("test json file created", function() {
     describe("generate test", function(){
         it("test it", function() {
-            assert.strictEqual("test","test");
             var inputFile = "../custom-functions-metadata/test/testfunctions.ts";
             var output = "./test.json";
             jsongenerator.generate(inputFile,output);
@@ -28,13 +14,21 @@ describe("test json file created", function() {
             assert.strictEqual(jsongenerator.errorFound, true, "error not created");
             var skipped = 'notadded';
             assert.strictEqual(jsongenerator.skippedFunctions[0],skipped, "skipped function not found");
-            //var e = jsongenerator.errorFound;
-            //console.log(e);
-            //console.log(jsongenerator.skippedFunctions)
-            //console.log(fs.existsSync(output));
-            //assert.equal(fs.existsSync(output), "json file not created");
-            //assert.ok(fs.existsSync(output), "json file not created2");
             assert.strictEqual(fs.existsSync(output), true, "json file not created");
+        });
+    });
+});
+describe("test errors", function() {
+    describe("failure to generate", function(){
+        it("test error", function() {
+             var inputFile = "../custom-functions-metadata/test/errorfunctions.js";
+            var output = "./errortest.json";
+            jsongenerator.generate(inputFile,output);
+            assert.strictEqual(jsongenerator.errorFound, true, "error not created");
+            var errorlog = jsongenerator.errorLogFile[1];
+            var errorstring = "Unsupported type in code comment:badtype";
+            assert.strictEqual(errorlog, errorstring, "Error string not found.");
+            assert.strictEqual(fs.existsSync(output), false, "json file created");
         });
     });
 });
