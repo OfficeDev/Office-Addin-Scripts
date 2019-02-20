3efad63217b4021099fa52eb6451026769eb8727
Adam Krantz <Adam.Krantz@microsoft.com>
2019-02-19 20:51:08 -0800

    separate lib from cli
    The package.json lib points to the main file when the package is imported,
    whereas the package.json bin refers to the file used when called
    from the command line (now called cli.js).

---
+++ packages/office-addin-debugging/src/cli.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as commander from "commander";
+import * as commands from "./commands";
+
+commander
+    .command("start <manifestPath> [appType]")
+    .option("--app <app>", "Specify which Office app to use.")
+    .option("--debug-method <method>", "The debug method to use.")
+    .option("--dev-server <command>", "Run the dev server.")
+    .option("--dev-server-port <port>", "Verify the dev server is running using this port.")
+    .option("--no-debug", "Start without debugging.")
+    .option("--no-live-reload", "Do not enable live-reload.")
+    .option("--packager <command>", "Run the packager.")
+    .option("--packager-host <host>")
+    .option("--packager-port <port>")
+    .option("--sideload <command>")
+    .option("--source-bundle-url-host <host>")
+    .option("--source-bundle-url-port <port>")
+    .option("--source-bundle-url-path <path>")
+    .option("--source-bundle-url-extension <extension>")
+    .action(commands.start);
+
+commander
+    .command("stop <manifestPath>")
+    .option("--unload <command>")
+    .action(commands.stop);
+
+commander.parse(process.argv);
