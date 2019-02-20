332dcb101f669890d07a600bc285da680ec6b670
Courtney Owen <cowen@microsoft.com>
2019-02-20 15:51:22 -0800

    Improve test infrastructure

--- packages/office-addin-test-infrastructure/src/office-addin-test-infrastructure.ts
+++ packages/office-addin-test-infrastructure/src/office-addin-test-infrastructure.ts
@@ -1,50 +1,59 @@
 import * as cors from "cors";
 import * as express from "express";
 import * as fs from "fs";
-let m_port: number = 0;
-let m_testServerStarted: boolean = false;
-let server: any;
 const app: any = express();
+let server: any;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-export default class officeAddinTestInfrastructure {
+export default class officeAddinTestServer {
+    m_port: number = 0;
+    m_testServerStarted: boolean = false;
+    m_jsonData: JSON = undefined;
+
     constructor(port: number) {
-        m_port = port;
+        this.m_port = port;
     }
 
     async startTestServer(): Promise<boolean> {
-        return new Promise<boolean>(async function (resolve, reject) {
-        process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-        const key = fs.readFileSync('certs/server.key');
-        const cert = fs.readFileSync('certs/server.crt');
-        const options = { key: key, cert: cert };
+        return new Promise<boolean>(async (resolve, reject) => {
+            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+            const key = fs.readFileSync('certs/server.key');
+            const cert = fs.readFileSync('certs/server.crt');
+            const options = { key: key, cert: cert };
 
-        app.use(cors());
-        app.get("/ping", function (req: any, res: any, next: any) {
-            res.send(process.platform === "win32" ? "Win32" : "Mac");
-            resolve(true);
-        });
-
-        const https = require("https");
-        server = https.createServer(options, app);
-
-        // listen for new web clients:
-        try {
-            server.listen(m_port, function () {
-                m_testServerStarted = true;
+            app.use(cors());
+            app.get("/ping", function (req: any, res: any, next: any) {
+                res.send(process.platform === "win32" ? "Win32" : "Mac");
                 resolve(true);
             });
-        } catch (err) {
-            reject(new Error(`Unable to start test server. \n${err}`));
-        }
-    });
+
+            app.post("/results", function (req: any, res: any) {
+                res.send("200");
+                this.m_jsonData = JSON.parse(req.query.data);
+                resolve(true);
+            }.bind(this));
+
+            const https = require("https");
+            server = https.createServer(options, app);
+
+            // listen for new web clients:
+            try {
+                server.listen(this.m_port, function () {
+                    this.m_testServerStarted = true;
+                    resolve(true);
+                }.bind(this));
+            } catch (err) {
+                reject(new Error(`Unable to start test server. \n${err}`));
+            }
+        });
     }
 
     async stopTestServer(): Promise<boolean> {
-        return new Promise<boolean>(async function (resolve, reject) {
-            if (m_testServerStarted) {
+        return new Promise<boolean>(async (resolve, reject) => {
+            if (this.m_testServerStarted) {
                 try {
                     server.close();
-                    m_testServerStarted = false;
+                    this.m_testServerStarted = false;
                     resolve(true);
                 } catch (err) {
                     reject(new Error(`Unable to stop test server. \n${err}`));
@@ -56,25 +65,52 @@ export default class officeAddinTestInfrastructure {
         });
     }
 
-    async getTestResults(): Promise<any> {
-        return new Promise<any>(async function (resolve, reject) {
-            try {
-                app.get("/results", function (req: any, res: any) {
-                    res.send("200");
-                    const jsonData: JSON = JSON.parse(req.query.data);
-                    resolve(jsonData);
-                });
-            } catch (err) {
-                reject(new Error(`Unable to get test results. \n${err}`));
-            }
-        });
+    getTestResults(): JSON {
+        return this.m_jsonData;
     }
 
     getTestServerState(): boolean {
-        return m_testServerStarted;
+        return this.m_testServerStarted;
     }
 
     getTestServerPort(): number {
-        return m_port;
+        return this.m_port;
     }
+}
+
+export async function pingTestServer(port: number): Promise < string > {
+    return new Promise<string>(async (resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        const pingUrl: string = `https://localhost:${port}/ping`;
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(xhr.responseText);
+            }
+            else if (xhr.readyState === 4 && xhr.status === 0 && xhr.responseText.indexOf("ECONNREFUSED") > 0) {
+                reject(xhr.responseText);
+            }
+        };
+        xhr.open("GET", pingUrl, true);
+        xhr.send();
+    });
+}
+
+export async function sendTestResults(data: any, port: number): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+        const json = JSON.stringify(data);
+        const xhr = new XMLHttpRequest();
+        const url: string = `https://localhost:${port}/results/`;
+        const dataUrl: string = url + "?data=" + encodeURIComponent(json);
+
+        xhr.open("POST", dataUrl, true);
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200 && xhr.responseText === "200") {
+                resolve(true);
+            }
+            else if (xhr.readyState === 4 && xhr.status === 0 && xhr.responseText.indexOf("ECONNREFUSED") > 0) {
+                reject(false);
+            }
+        };
+    });
 }
--- packages/office-addin-test-infrastructure/test/test.ts
+++ packages/office-addin-test-infrastructure/test/test.ts
@@ -1,66 +1,47 @@
 import * as assert from "assert";
-import officeAddinTestInfrastructure from "../src/office-addin-test-infrastructure";
+import officeAddinTestServer from "../src/office-addin-test-infrastructure";
+import * as testHelpers from "../src/office-addin-test-infrastructure";
 const port: number = 8080;
-const officeAddinTestInfra = new officeAddinTestInfrastructure(port);
+const testServer = new officeAddinTestServer(port);
 const testKey: string = "TestString";
 const testValue: string = "Office-Addin-Test-Infrastructure";
 const testValues: any = [];
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-
-describe("1) Setup test server, 2) ping server for response, 3) send data to server and get results, 4) stop test server", function() {
-    describe("Start sideload, start dev-server, and start test-server", function() {
+describe("End-to-end validation of test server", function() {
+    describe("1) Setup test server, 2) ping server for response, 3) send data to server and get results, 4) stop test server", function() {
         it("Dev-server should have started", async function() {
-            const startTestServer = await officeAddinTestInfra.startTestServer();
+            const startTestServer = await testServer.startTestServer();
             assert.equal(startTestServer, true);
-            assert.equal(officeAddinTestInfra.getTestServerState(), true);
-            assert.equal(officeAddinTestInfra.getTestServerPort(), port);
+            assert.equal(testServer.getTestServerState(), true);
+            assert.equal(testServer.getTestServerPort(), port);
         });
         it("Test server should have responded to ping", async function() {
-            const testServerResponse = await _pingTestServer();
+            const testServerResponse = await testHelpers.pingTestServer(port);
             assert.equal(testServerResponse, process.platform === "win32" ? "Win32" : "Mac");
         });
         it("Test server should have received sent data and getTestResults should have retrieved data ", async function() {
-            _sendTestData();
-            const testResults: any = await officeAddinTestInfra.getTestResults();
-            assert.equal(testResults[0].Name, testKey);
-            assert.equal(testResults[0].Value, testValue);
+            const sendData = await _sendTestData();
+            const getResults: JSON = testServer.getTestResults();
+            assert.equal(sendData, true);
+            assert.equal(getResults[0].Name, testKey);
+            assert.equal(getResults[0].Value, testValue);
         });
         it("Test server should have stopped ", async function () {
-            const stopTestServer = await officeAddinTestInfra.stopTestServer();
+            const stopTestServer = await testServer.stopTestServer();
             assert.equal(stopTestServer, true);
-            assert.equal(officeAddinTestInfra.getTestServerState(), false);
+            assert.equal(testServer.getTestServerState(), false);
         });
     });
 });
 
-async function _pingTestServer(): Promise<string> {
-    return new Promise<string>(async function(resolve) {
-        const xhr = new XMLHttpRequest();
-        const pingUrl: string = `https://localhost:${port}/ping`;
-        xhr.onreadystatechange = (e) => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                resolve(xhr.responseText);
-            }
-        };
-        xhr.open("GET", pingUrl, true);
-        xhr.send();
-    });
-}
-
-async function _sendTestData(): Promise<void> {
+async function _sendTestData(): Promise<boolean> {
     const testData: any = {};
     const nameKey = "Name";
     const valueKey = "Value";
+
     testData[nameKey] = testKey;
     testData[valueKey] = testValue;
     testValues.push(testData);
 
-    const json = JSON.stringify(testValues);
-    const xhr = new XMLHttpRequest();
-    const url: string = `https://localhost:${port}/results/`;
-    const dataUrl: string = url + "?data=" + encodeURIComponent(json);
-
-    xhr.open("GET", dataUrl, true);
-    xhr.send();
+    return testHelpers.sendTestResults(testValues, port);
 }
