5b0df1646db852168aa3ba5e0c577069f7f436a2
Sachin Arunkumar <saarunku@microsoft.com>
2019-03-13 10:41:14 -0700

    removed dependency for fs-path

--- packages/office-addin-dev-certs/src/generate.ts
+++ packages/office-addin-dev-certs/src/generate.ts
@@ -1,22 +1,39 @@
+import * as fs from "fs";
 import * as mkcert from "mkcert";
+import * as mkdirp from "mkdirp";
+import * as path from "path";
 import * as defaults from "./defaults";
 import * as install from "./install";
 import * as verify from "./verify";
+
+function createDirectoryIfNotExists(certificatePath: string) {
+    const certificateDir = path.dirname(certificatePath);
+    if (!fs.existsSync(certificateDir)) {
+        mkdirp(certificateDir, function(err) {
+        if (err) {
+            console.log("Failed to create directory " + err);
+        } else {
+            console.log("Sucessfully created a directory " + certificateDir);
+        }
+        });
+    }
+  }
 
 /* Generate operation will check if there is already valid certificate installed.
    if yes, then this operation will be no op.
    else, new certificates are generated and installed if --install was provided.
 */
-export async function generateCertificates(caCertificatePath: string = defaults.caCertificatePath, certPath: string = defaults.localhostCertificatePath, keyPath: string = defaults.localhostKeyPath, installCaCertificate: boolean = false) {
+export async function generateCertificates(caCertificatePath: string = defaults.caCertificatePath, localhostCertificatePath: string = defaults.localhostCertificatePath, localhostKeyPath: string = defaults.localhostKeyPath, installCaCertificate: boolean = false) {
 
     const isCertificateValid  = verify.verifyCaCertificate();
     if (isCertificateValid) {
         console.log("A valid CA certificate is already installed.");
         return;
     }
-
-    const fs = require("fs-path");
-    const caCertificate= await mkcert.createCA({
+    createDirectoryIfNotExists(caCertificatePath);
+    createDirectoryIfNotExists(localhostCertificatePath);
+    createDirectoryIfNotExists(localhostKeyPath);
+    const caCertificate = await mkcert.createCA({
                             countryCode: defaults.countryCode,
                             locality: defaults.locality,
                             organization: defaults.certificateName,
@@ -30,8 +47,8 @@ export async function generateCertificates(caCertificatePath: string = defaults.
                                         validityDays: defaults.daysUntilCertificateExpires,
                                     });
     fs.writeFileSync(`${caCertificatePath}`, caCertificate.cert);
-    fs.writeFileSync(`${certPath}`, localhostCertificate.cert);
-    fs.writeFileSync(`${keyPath}`, localhostCertificate.key);
+    fs.writeFileSync(`${localhostCertificatePath}`, localhostCertificate.cert);
+    fs.writeFileSync(`${localhostKeyPath}`, localhostCertificate.key);
     if (caCertificatePath === defaults.caCertificatePath) {
         console.log("The developer certificates have been generated in " + process.cwd());
     } else {
