4edc80d480bcdf8aa7fdd7d8bfe363a1a47dc804
Courtney Owen <cowen@microsoft.com>
2019-03-21 09:43:47 -0700

    Cleaned up test helper methods and made them more robust

--- packages/office-addin-test-helpers/src/testHelpers.ts
+++ packages/office-addin-test-helpers/src/testHelpers.ts
@@ -2,19 +2,17 @@
 import * as childProcess from "child_process";
 import * as cps from "current-processes";
 import * as fetch from "isomorphic-fetch";
-
+let applicationSideloaded: boolean = false;
+let devServerProcess: any;
 let devServerStarted: boolean = false;
 let port: number = 8080;
-var subProcess: any;
 
 export async function pingTestServer(portNumber: number | undefined): Promise<Object> {
     return new Promise<Object>(async (resolve, reject) => {
         if (portNumber !== undefined) {
             port = portNumber;
         }
-
         const serverResponse: any = {};
-
         try {
             const pingUrl: string = `https://localhost:${port}/ping`;
             const response = await fetch(pingUrl);
@@ -29,12 +27,11 @@ export async function pingTestServer(portNumber: number | undefined): Promise<Ob
     });
 }
 
-export async function sendTestResults(data: Object, portNumber: number | undefined): Promise<boolean> {
+export async function sendTestResults(data: object, portNumber: number | undefined): Promise<boolean> {
     return new Promise<boolean>(async (resolve, reject) => {
         if (portNumber !== undefined) {
             port = portNumber;
         }
-
         const json = JSON.stringify(data);
         const url: string = `https://localhost:${port}/results/`;
         const dataUrl: string = url + "?data=" + encodeURIComponent(json);
@@ -44,7 +41,7 @@ export async function sendTestResults(data: Object, portNumber: number | undefin
                 method: 'post',
                 body: JSON.stringify(data),
                 headers: { 'Content-Type': 'application/json' },
-            })
+            });
             resolve(true);
         } catch (err) {
             reject(false);
@@ -55,15 +52,14 @@ export async function sendTestResults(data: Object, portNumber: number | undefin
 
 export async function sideloadDesktopApp(application: string, manifestPath: string): Promise<boolean> {
     return new Promise<boolean>(async function (resolve, reject) {
-
         if (process.platform !== 'win32' && process.platform !== 'darwin') {
-            reject(false);
+            resolve(false);
         }
 
         try {
             const cmdLine = `office-toolbox sideload -m ${manifestPath} -a ${application}`;
-            const sideloadSucceeded = await _executeCommandLine(cmdLine);
-            resolve(sideloadSucceeded);
+            applicationSideloaded = await executeCommandLine(cmdLine);
+            resolve(applicationSideloaded);
         } catch (err) {
             reject(err);
         }
@@ -71,23 +67,24 @@ export async function sideloadDesktopApp(application: string, manifestPath: stri
 }
 
 export async function startDevServer(): Promise<boolean> {
-    devServerStarted = false;
-    const cmdLine = "npm run dev-server";
-    subProcess = childProcess.spawn(cmdLine, [], {
-        detached: true,
-        shell: true,
-        stdio: "ignore"
-    });
-    subProcess.on("error", (err) => {
-        console.log(`Unable to run command: ${cmdLine}.\n${err}`);
-    });
+    return new Promise<boolean>((resolve, reject) => {
+        devServerStarted = false;
+        const cmdLine = "npm run dev-server";
+        devServerProcess = childProcess.spawn(cmdLine, [], {
+            detached: true,
+            shell: true,
+            stdio: "ignore"
+        });
+        devServerProcess.on("error", (err) => {
+            reject(err);
+        });
 
-    devServerStarted = subProcess.pid != undefined;
-
-    return devServerStarted;
+        devServerStarted = devServerProcess.pid != undefined;
+        resolve(devServerStarted);
+    });
 }
 
-async function _executeCommandLine(cmdLine: string): Promise<boolean> {
+async function executeCommandLine(cmdLine: string): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
         childProcess.exec(cmdLine, (error) => {
             if (error) {
@@ -100,69 +97,92 @@ async function _executeCommandLine(cmdLine: string): Promise<boolean> {
 }
 
 // Office-JS close workbook API is coming soon.  Once it's available we can stript out this code to kill the Excel process
-export async function teardownTestEnvironment(application: string): Promise<void> {
+export async function teardownTestEnvironment(application: string): Promise<boolean> {
+    return new Promise<boolean>(async function (resolve, reject) {
+        let applicationKilled: boolean = false;
+        let devServerKilled: boolean = false;
 
-    await closeDesktopApplication(application);
-
-    // if the dev-server was started, kill the spawned process
-    if (devServerStarted) {
-        if (process.platform == "win32") {
-            childProcess.spawn("taskkill", ["/pid", subProcess.pid, '/f', '/t']);
-        } else {
-            subProcess.kill();
+        if (applicationSideloaded) {
+            try {
+                applicationKilled = await closeDesktopApplication(application);
+            } catch (err) {
+                reject(`Test teardown failed: ${err}`);
+            }            
         }
-    }
-}
 
-async function closeDesktopApplication(application: string): Promise<void> {
-    let processName: string = "";
-    switch (application.toLowerCase()) {
-        case "excel":
-            processName = "Excel";
-            break;
-        case "powerpoint":
-            processName = "Powerpnt";
-            break;
-        case "onenote":
-            processName = "Onenote";
-            break;
-        case "outlook":
-            processName = "Outlook";
-            break;
-        case "project":
-            processName = "Project";
-            break;
-        case "word":
-            processName = "Winword";
-            break;
-        default:
-            throw new Error(`${application} is not a valid Office desktop application.`);
-    }
-
-    try {
-        if (process.platform == "win32") {
-            const cmdLine = `tskill ${processName}`;
-            await _executeCommandLine(cmdLine);
-        } else {
-            const pid = await _getProcessId(processName);
-            if (pid != undefined) {
-                process.kill(pid);
+        // if the dev-server was started, kill the spawned process
+        if (devServerStarted) {
+            try {
+                if (process.platform == "win32") {
+                    childProcess.spawn("taskkill", ["/pid", devServerProcess.pid, '/f', '/t']);
+                } else {
+                    devServerProcess.kill();
+                }                
+                devServerKilled = true;
+            } catch (err) {
+                reject(`Test teardown failed: ${err}`);
             }
         }
-    } catch (err) {
-        console.log(`Unable to kill ${application} process. ${err}`);
-    }
-
+        resolve(applicationKilled && devServerKilled);
+    });
 }
 
-async function _getProcessId(processName: string): Promise<number> {
+// Office-JS close workbook API is coming soon.  Once it's available we can stript out this code to kill the Excel process
+async function closeDesktopApplication(application: string): Promise<boolean> {
+    return new Promise<boolean>(async function (resolve, reject) {
+        let processName: string = "";
+        switch (application.toLowerCase()) {
+            case "excel":
+                processName = "Excel";
+                break;
+            case "powerpoint":
+                processName = "Powerpnt";
+                break;
+            case "onenote":
+                processName = "Onenote";
+                break;
+            case "outlook":
+                processName = "Outlook";
+                break;
+            case "project":
+                processName = "Project";
+                break;
+            case "word":
+                processName = "Winword";
+                break;
+            default:
+                reject(`${application} is not a valid Office desktop application.`);
+            }
+
+        try {
+            let appClosed: boolean = false;
+            if (process.platform == "win32") {
+                const cmdLine = `tskill ${processName}`;
+                appClosed = await executeCommandLine(cmdLine);
+            } else {
+                const pid = await getProcessId(processName);
+                if (pid != undefined) {
+                    process.kill(pid);
+                    appClosed = true;
+                } else {
+                    resolve(false);
+                }               
+            }
+            resolve(appClosed);
+        } catch (err) {
+            reject(`Unable to kill ${application} process. ${err}`);
+        }
+    });
+}
+
+async function getProcessId(processName: string): Promise<number> {
     return new Promise<number>(async function (resolve, reject) {
         cps.get(function (err: Error, processes: any) {
             try {
-                const p = processes.filter(function (p: any) {
+                const processArray = processes.filter(function (p: any) {
                     return (p.name.indexOf(processName) > 0);
                 });
-                resolve(p.length > 0 ? p[0].pid : undefined);
+                resolve(processArray.length > 0 ? processArray[0].pid : undefined);
             }
             catch (err) {
                 reject(err);
