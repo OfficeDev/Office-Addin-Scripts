623959c8582587bca8d0ba6d326d1292a06fa519
Adam Krantz <Adam.Krantz@microsoft.com>
2019-03-21 07:01:05 -0700

    add test case: return-type
    Adding this test case found problems. Remove "skip" file to run the test.

---
+++ packages/custom-functions-metadata/test/cases/return-type/functions.ts
@@ -0,0 +1,62 @@
+/**
+ * @customfunction
+ */
+function returnsBoolean(): boolean {
+  return true;
+}
+
+/**
+ * @customfunction
+ */
+function returnsNumber(): number {
+  return 5;
+}
+
+/**
+ * @customfunction
+ */
+function returnsString(): string {
+  return "abc";
+}
+
+/**
+ * @customfunction
+ */
+function returnsObject(): object {
+  return {};
+}
+
+/**
+ * @customfunction
+ */
+function returnsArray(): string[] {
+  return ["abc"];
+}
+
+/**
+ * @customfunction
+ */
+function returnsMatrixAny(): any[][] {
+  return [[1, "string"]];
+}
+
+/**
+ * @customfunction
+ */
+function returnsMatrixBoolean(): boolean[][] {
+  return [[true]];
+}
+
+/**
+ * @customfunction
+ */
+function returnsMatrixNumber(): number[][] {
+  return [[5]];
+}
+
+/**
+ * @customfunction
+ */
+function returnsMatrixString(): string[][] {
+  return [["abc"]];
+}
