71bb46175e9b23a01fe6922407cd724c5eee3ddd
Adam Krantz <Adam.Krantz@microsoft.com>
2019-03-21 07:00:44 -0700

    add trailing newline

---
+++ packages/custom-functions-metadata/test/cases/return-type/functions.js
@@ -0,0 +1,71 @@
+/**
+ * @customfunction
+ * @returns {boolean}
+ */
+function returnsBoolean() {
+  return true;
+}
+
+/**
+ * @customfunction
+ * @returns {number}
+ */
+function returnsNumber() {
+  return 5;
+}
+
+/**
+ * @customfunction
+ * @returns {string}
+ */
+function returnsString() {
+  return "abc";
+}
+
+/**
+ * @customfunction
+ * @returns {object}
+ */
+function returnsObject() {
+  return {};
+}
+
+/**
+ * @customfunction
+ * @returns {string[]}
+ */
+function returnsArray() {
+  return ["abc"];
+}
+
+/**
+ * @customfunction
+ * @returns {any[][]}
+ */
+function returnsMatrixAny() {
+  return [[1, "string"]];
+}
+
+/**
+ * @customfunction
+ * @returns {boolean[][]}
+ */
+function returnsMatrixBoolean() {
+  return [[true]];
+}
+
+/**
+ * @customfunction
+ * @returns {number[][]}
+ */
+function returnsMatrixNumber() {
+  return [[5]];
+}
+
+/**
+ * @customfunction
+ * @returns {string[][]}
+ */
+function returnsMatrixString() {
+  return [["abc"]];
+}
