2a6a0c72a562de8a7b2dea77e242a819632c1d98
Sachin Arunkumar <saarunku@microsoft.com>
2019-03-22 14:29:28 -0700

    removed customization for install

--- packages/office-addin-dev-certs/src/cli.ts
+++ packages/office-addin-dev-certs/src/cli.ts
@@ -9,10 +9,6 @@ import * as defaults from "./defaults";
 
 commander
     .command("install")
-    .option("--ca-cert <ca-cert-path>", `Specifies the path where the CA certificate file is written. Default: ${defaults.caCertificatePath}`)
-    .option("--cert <cert-path>", `Specifies the path where the SSL certificate file is written. Default: ${defaults.localhostCertificatePath}`)
-    .option("--key <key-path>", `Specifies the path where the private key for the SSL certificate file is written. Default: ${defaults.localhostKeyPath}`)
-    .option("--days <days>", `Specifies the validity of CA certificate in days. Default: ${defaults.daysUntilCertificateExpires}`)
     .description(`Generate an SSL certificate for "localhost" issued by a CA certificate which is installed.`)
     .action(commands.install);
 
