12a9b4a82b86798f6c293da9767f254c84d84442
Courtney Owen <38081557+TCourtneyOwen@users.noreply.github.com>
2019-03-21 17:57:19 -0700

    Office addin test infrastructure (#63)
    Create test infrastructure package that our Office Add-in templates can use for testing.

---
+++ packages/office-addin-test-server/src/commands.ts
@@ -0,0 +1,56 @@
+import * as commnder from "commander";
+import { TestServer } from "./testServer"
+const defaultPort: number = 8080
+
+export async function start(command: commnder.Command) {
+    const testServerPort: number = (command.port !== undefined) ? parseTestServerPort(command.port) : defaultPort;
+    const testServer = new TestServer(testServerPort);
+    const serverStarted: boolean = await testServer.startTestServer();
+
+    if (serverStarted){
+        console.log(`Server started successfully on port ${testServerPort}`);
+    }
+    else{
+        console.log("Server failed to start");
+    }
+}
+
+function parseNumericCommandOption(optionValue: any, errorMessage: string = "The value should be a number."): number {
+    switch (typeof (optionValue)) {
+        case "number": {
+            return optionValue;
+        }
+        case "string": {
+            let result: number;
+
+            try {
+                result = parseInt(optionValue, 10);
+            } catch (err) {
+                throw new Error(errorMessage);
+            }
+
+            if (Number.isNaN(result)) {
+                throw new Error(errorMessage);
+            }
+
+            return result;
+        }
+        case "undefined": {
+            return undefined;
+        }
+        default: {
+            throw new Error(errorMessage);
+        }
+    }
+}
+
+function parseTestServerPort(optionValue: any): number {
+    const testServerPort = parseNumericCommandOption(optionValue, "--dev-server-port should specify a number.");
+
+    if (testServerPort !== undefined) {
+        if ((testServerPort < 0) || (testServerPort > 65535)) {
+            throw new Error("port should be between 0 and 65535.");
+        }
+    }
+    return testServerPort;
+}
