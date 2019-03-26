914f4e9102819e51da7eb1850dcef5a9c82f11ef
Adam Krantz <Adam.Krantz@microsoft.com>
2019-03-26 09:55:08 -0700

    allow platform-specific expected metadata

--- packages/custom-functions-metadata/test/src/testCases.ts
+++ packages/custom-functions-metadata/test/src/testCases.ts
@@ -10,6 +10,15 @@ function deleteFileIfExists(filePath: string): void {
     }
 }
 
+function getPlatformSuffix(): string {
+    switch (process.platform) {
+        case "darwin":
+            return "mac";
+        default:
+            return process.platform;
+    }
+}
+
 function normalizeLineEndings(text: string | undefined): string | undefined {    
     return text ? text.replace(/\r\n|\r/g, "\n") : text;
 }
@@ -38,8 +47,9 @@ describe("test cases", function() {
                         const actualErrorsFile = path.join(testCaseDirPath, `actual.${scriptType}.errors.txt`);
                         const expectedErrorsFile = path.join(testCaseDirPath, `expected.${scriptType}.errors.txt`);
                         const actualMetadataFile = path.join(testCaseDirPath, `actual.${scriptType}.json`);
-                        const expectedMetadataFile = path.join(testCaseDirPath, "expected.json");
-                        const expectedMetadata: string | undefined = readFileIfExists(expectedMetadataFile);
+                        const expectedMetadata: string | undefined =
+                            readFileIfExists(path.join(testCaseDirPath, `expected.${getPlatformSuffix()}.json`))
+                            || readFileIfExists(path.join(testCaseDirPath, "expected.json"));
 
                         // add a file named "debugger" to break on the test case
                         if (fs.existsSync(path.resolve(testCaseDirPath, "debugger"))) {
