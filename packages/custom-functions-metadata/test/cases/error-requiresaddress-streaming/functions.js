271f9b7ebddd563d772d00701a97870c60b44d67
Don Gunther <donaldgu@microsoft.com>
2019-04-10 10:49:09 -0700

    add test for requiresaddress streaming error

---
+++ packages/custom-functions-metadata/test/cases/error-requiresaddress-streaming/functions.js
@@ -0,0 +1,10 @@
+/**
+ * Test the new streaming invocation type
+ * @param x {string} string
+ * @param stream {CustomFunctions.StreamingInvocation<string>} StreamingInvocation type
+ * @customfunction
+ * @requiresaddress
+ */
+function customFunctionStreamingInvocationTest(x, stream){
+    // Empty
+}
---
+++ packages/custom-functions-metadata/test/cases/error-requiresaddress-streaming/functions.ts
@@ -0,0 +1,10 @@
+/**
+ * Test the new streaming invocation type
+ * @param x string
+ * @param stream StreamingInvocation type
+ * @customfunction
+ * @requiresaddress
+ */
+function customFunctionStreamingInvocationTest(x: string, stream: CustomFunctions.StreamingInvocation<string>){
+    // Empty
+}
