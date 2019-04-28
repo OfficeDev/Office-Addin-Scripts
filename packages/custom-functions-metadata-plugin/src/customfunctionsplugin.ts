b8ad616de4b0d0114efd3ce4b32206adf4ece97f
Don Gunther <donaldgu@microsoft.com>
2019-04-28 08:01:03 -0700

    handle inputfile name

--- packages/custom-functions-metadata-plugin/src/customfunctionsplugin.ts
+++ packages/custom-functions-metadata-plugin/src/customfunctionsplugin.ts
@@ -17,6 +17,7 @@ class CustomFunctionsMetadataPlugin {
     public apply(compiler: webpack.Compiler) {
         const outputPath: string = (compiler.options && compiler.options.output) ? compiler.options.output.path || "" : "";
         const outputFilePath = path.resolve(outputPath, this.options.output);
+        const inputFilePath = path.resolve(this.options.input);
         let errors: string[];
         let associate: metadata.IAssociate[];
 
@@ -28,7 +29,7 @@ class CustomFunctionsMetadataPlugin {
                     throw err;
                 }
             }
-            const generateResult = await metadata.generate(this.options.input, outputFilePath, true);
+            const generateResult = await metadata.generate(inputFilePath, outputFilePath, true);
             errors = generateResult.errors;
             associate = generateResult.associate;
         });
@@ -36,7 +37,7 @@ class CustomFunctionsMetadataPlugin {
         compiler.hooks.emit.tap(pluginName, (compilation) => {
             if (errors.length > 0) {
                 compilation.errors.push("Generating metadata file:" + outputFilePath);
-                errors.forEach((err: string) => compilation.errors.push(this.options.input + " " + err));
+                errors.forEach((err: string) => compilation.errors.push(inputFilePath + " " + err));
             } else {
                 const stats = fs.statSync(outputFilePath);
                 const content = fs.readFileSync(outputFilePath);
@@ -50,7 +51,7 @@ class CustomFunctionsMetadataPlugin {
         let functionsUpdated: boolean = false;
         compiler.hooks.compilation.tap(pluginName, (compilation, params) => {
             compilation.moduleTemplates.javascript.hooks.render.tap(pluginName, (s, m) => {
-                if (m._source && m._source._name.endsWith("functions.ts") && !functionsUpdated) {
+                if (m._source && m._source._name.endsWith(inputFilePath) && !functionsUpdated) {
                     associate.forEach((item) => {
                         m._source._value += `\nCustomFunctions.associate("${item.id}",${item.functionName});`;
                       });
