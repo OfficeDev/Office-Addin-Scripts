1f3683bd54d4f139b85ea6a12e56ac6bf4df1caa
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-19 20:41:06 -0700

    custom-functions-metadata: show help if no args; add --version option

--- packages/custom-functions-metadata/src/cli.ts
+++ packages/custom-functions-metadata/src/cli.ts
@@ -6,9 +6,16 @@
 import * as commander from "commander";
 import * as commands from "./commands";
 
+commander.name("custom-functions-metadata");
+commander.version(process.env.npm_package_version || "(version not available)");
+
 commander
   .command("generate <source-file> <metadata-file>")
   .description("Generate the metadata for the custom functions from the source code.")
   .action(commands.generate);
 
-commander.parse(process.argv);
+if (process.argv.length > 2) {
+    commander.parse(process.argv);
+} else {
+    commander.help();
+}
