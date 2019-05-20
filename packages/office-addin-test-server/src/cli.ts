b4d81ecf77db43de85190474d7a4fa3996c739da
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-19 21:29:07 -0700

    office-addins-test-server: show help if no args; add --version option

--- packages/office-addin-test-server/src/cli.ts
+++ packages/office-addin-test-server/src/cli.ts
@@ -2,9 +2,16 @@ import * as commander from "commander";
 import * as commands from "./commands";
 import { defaultPort } from "./testServer";
 
+commander.name("office-addin-test-server");
+commander.version(process.env.npm_package_version || "(version not available)");
+
 commander
     .command("start")
     .option(`-p --port [port number]", "Port number must be between 0 - 65535. Default: ${defaultPort}`)
     .action(commands.start);
 
-commander.parse(process.argv);
+if (process.argv.length > 2) {
+    commander.parse(process.argv);
+} else {
+    commander.help();
+}
