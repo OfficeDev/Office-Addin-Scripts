e9f401648a0f432717198c514dd362bec934d163
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-28 14:17:35 -0700

    revert bin config change

--- packages/office-addin-cli/cli.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-//
-// If the package.json bin config specifies a file in the lib folder, it will cause an 
-// error during "npm install" if the lib folder doesn't exist (because the package hasn't been built yet).
-// It specifies this file instead which then calls into the file in the lib folder.
-require("./lib/cli.js");
