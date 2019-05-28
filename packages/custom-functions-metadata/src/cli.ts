9e8d1a48b27d2421d3d394d34afdd39a5f4b864e
Adam Krantz <Adam.Krantz@microsoft.com>
2019-05-28 08:25:31 -0700

    show help if no args; add --version (#130)
    * show help if no args; add --version option
    * show error message if command is not valid

--- packages/custom-functions-metadata/src/cli.ts
+++ packages/custom-functions-metadata/src/cli.ts
@@ -4,11 +4,26 @@
 // Licensed under the MIT license.
 
 import * as commander from "commander";
+import { logErrorMessage } from "office-addin-cli";
 import * as commands from "./commands";
+
+commander.name("custom-functions-metadata");
+commander.version(process.env.npm_package_version || "(version not available)");
 
 commander
   .command("generate <source-file> <metadata-file>")
   .description("Generate the metadata for the custom functions from the source code.")
   .action(commands.generate);
 
-commander.parse(process.argv);
+// if the command is not known, display an error
+commander.on("command:*", function() {
+  logErrorMessage(`The command syntax is not valid.\n`);
+  process.exitCode = 1;
+  commander.help();
+});
+
+if (process.argv.length > 2) {
+    commander.parse(process.argv);
+} else {
+    commander.help();
+}
