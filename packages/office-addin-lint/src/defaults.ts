864111a0ecc666929e2f4afaf6b9cdeddfec0c86
Artur Tarasenko <Artur.Tarasenko@Microsoft.com>
2021-01-19 14:22:25 -0800

    Putting office-addin-lint usage data changes in separate branch to make integration easier

--- packages/office-addin-lint/src/defaults.ts
+++ packages/office-addin-lint/src/defaults.ts
@@ -3,11 +3,11 @@
 
 import * as usageData from "office-addin-usage-data";
 
-export const lintFiles = "src/**/*.{ts,tsx,js,jsx}";
+export const lintFiles = "src\\**\\*.{ts,tsx,js,jsx}";
 
 // Usage data defaults
 export const usageDataObject: usageData.OfficeAddinUsageData = new usageData.OfficeAddinUsageData({
-    projectName: "office-addin-lint",
-    instrumentationKey: usageData.instrumentationKeyForOfficeAddinCLITools,
-    raisePrompt: false
+  projectName: "office-addin-lint",
+  instrumentationKey: usageData.instrumentationKeyForOfficeAddinCLITools,
+  raisePrompt: false
 });
