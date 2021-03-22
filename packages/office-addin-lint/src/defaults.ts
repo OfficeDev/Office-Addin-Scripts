ada0031d5c693f8955a28161a978a90f0110a64e
Adam Krantz <Adam.Krantz@microsoft.com>
2021-03-22 14:31:07 -0700

    office-addin-lint: update eslint-config-prettier (#406)

--- packages/office-addin-lint/src/defaults.ts
+++ packages/office-addin-lint/src/defaults.ts
@@ -3,23 +3,23 @@
 
 import * as usageData from "office-addin-usage-data";
 
-export const lintFiles = "src/**/*.{ts,tsx,js,jsx}";
+export const lintFiles = "src/**/*.{ts?(x),js?(x)}";
 
 export enum ESLintExitCode {
   NoLintErrors = 0,
   HasLintError = 1,
-  CommandFailed = 2
+  CommandFailed = 2,
 }
 
 export enum PrettierExitCode {
   NoFormattingProblems = 0,
   HasFormattingProblem = 1,
-  CommandFailed = 2
+  CommandFailed = 2,
 }
 
 // Usage data defaults
 export const usageDataObject: usageData.OfficeAddinUsageData = new usageData.OfficeAddinUsageData({
   projectName: "office-addin-lint",
   instrumentationKey: usageData.instrumentationKeyForOfficeAddinCLITools,
-  raisePrompt: false
+  raisePrompt: false,
 });
