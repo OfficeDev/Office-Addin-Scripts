77a85a586304e8acc3dddc180b971e925ec2ece6
Darren Miller <darrmill@microsoft.com>
2021-05-14 17:04:26 -0700

    Corrected rule export in plugin and tested linting against plugin code

--- packages/eslint-plugin-office-addins/src/main.ts
+++ packages/eslint-plugin-office-addins/src/main.ts
@@ -1,44 +1,33 @@
+import rules from "./rules";
+
 module.exports = {
-    rules: {
-        'no-context-sync-in-loop': require('./rules/no-context-sync-in-loop'),
+  rules,
+  configs: {
+    recommended: {
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint", "office-addins", "prettier"],
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
+      extends: ["eslint:recommended"],
+      rules: {
+        "prettier/prettier": ["error", { endOfLine: "auto" }],
+        "no-eval": "error",
+        "no-delete-var": "warn",
+        "no-octal": "warn",
+        "no-inner-declarations": "warn",
+      },
     },
-    configs: {
-        recommended: {
-            parser: "@typescript-eslint/parser",
-            plugins: [
-                '@typescript-eslint',
-                'office-addins',
-                'prettier',
-            ],
-            parserOptions: {
-                ecmaVersion: 6,
-                sourceType: "module",
-                ecmaFeatures: {
-                    jsx: true
-                },
-                project: "./tsconfig.json"
-            },
-            extends: ['eslint:recommended'],
-            rules: {
-                'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
-                'no-eval': 'error',
-                'no-delete-var': 'warn',
-                'no-octal': 'warn',
-                'no-inner-declarations': 'warn',
-            }
-        },
-        react: {
-            extends: [
-                'plugin:office-addins/recommended',
-                'plugin:react/recommended',
-            ]
-        },
-        reactnative: {
-            extends: [
-                'plugin:office-addins/recommended',
-                'plugin:react-native/all',
-            ],
-        }
-    }
-  };
-  
+    react: {
+      extends: ["plugin:office-addins/recommended", "plugin:react/recommended"],
+    },
+    reactnative: {
+      extends: ["plugin:office-addins/recommended", "plugin:react-native/all"],
+    },
+  },
+};
---
+++ packages/eslint-plugin-office-addins/src/rules/index.ts
@@ -0,0 +1,5 @@
+import noContextSyncInLoop from "./no-context-sync-in-loop";
+
+export default {
+  "no-context-sync-in-loop": noContextSyncInLoop,
+};
