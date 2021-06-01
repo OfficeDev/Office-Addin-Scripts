ac1e9d32446af5aaa3f3b8afdc217b532ed3c711
igor-ribeiiro <igormr98mr@gmail.com>
2021-06-01 13:59:01 -0700

    Adding code from eslint

--- packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
+++ packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
@@ -1,5 +1,68 @@
 import { TSESTree } from "@typescript-eslint/typescript-estree";
 
+
+const SENTINEL_TYPE = /^(?:(?:Function|Class)(?:Declaration|Expression)|ArrowFunctionExpression|CatchClause|ImportDeclaration|ExportNamedDeclaration)$/u;
+const FOR_IN_OF_TYPE = /^For(?:In|Of)Statement$/u;
+
+/**
+ * Checks whether or not a given location is inside of the range of a given node.
+ * @param {ASTNode} node An node to check.
+ * @param {number} location A location to check.
+ * @returns {boolean} `true` if the location is inside of the range of the node.
+ */
+ function isInRange(node: any, location: number): boolean {
+  return node && node.range[0] <= location && location <= node.range[1];
+}
+
+/**
+ * Checks whether or not a given reference is inside of the initializers of a given variable.
+ *
+ * This returns `true` in the following cases:
+ *
+ *     var a = a
+ *     var [a = a] = list
+ *     var {a = a} = obj
+ *     for (var a in a) {}
+ *     for (var a of a) {}
+ * @param {Variable} variable A variable to check.
+ * @param {Reference} reference A reference to check.
+ * @returns {boolean} `true` if the reference is inside of the initializers.
+ */
+ function isInInitializer(variable: any, reference: any): boolean {
+  if (variable.scope !== reference.from) {
+      return false;
+  }
+
+  let node = variable.identifiers[0].parent;
+  const location = reference.identifier.range[1];
+
+  while (node) {
+      if (node.type === "VariableDeclarator") {
+          if (isInRange(node.init, location)) {
+              return true;
+          }
+          if (FOR_IN_OF_TYPE.test(node.parent.parent.type) &&
+              isInRange(node.parent.parent.right, location)
+          ) {
+              return true;
+          }
+          break;
+      } else if (node.type === "AssignmentPattern") {
+          if (isInRange(node.right, location)) {
+              return true;
+          }
+      } else if (SENTINEL_TYPE.test(node.type)) {
+          break;
+      }
+
+      node = node.parent;
+  }
+
+  return false;
+}
+
+
+
 export = {
   name: "load-object-before-read",
   meta: {
@@ -16,6 +79,50 @@ export = {
     schema: [],
   },
   create: function (context: any) {
+      /**
+      * Finds and validates all variables in a given scope.
+      * @param {Scope} scope The scope object.
+      * @returns {void}
+      * @private
+      */
+      function findVariablesInScope(scope: any) {
+        scope.references.forEach((reference: { resolved: any; init: any; identifier: { range: number[]; }; }) => {
+          const variable = reference.resolved;
+
+          /*
+            * Skips when the reference is:
+            * - initialization's.
+            * - referring to an undefined variable.
+            * - referring to a global environment variable (there're no identifiers).
+            * - located preceded by the variable (except in initializers).
+            */
+          if (reference.init ||
+              !variable ||
+              variable.identifiers.length === 0 ||
+              (variable.identifiers[0].range[1] < reference.identifier.range[1] && !isInInitializer(variable, reference))
+            ) {
+              return;
+          }
+
+          // Reports.
+          context.report({
+              node: reference.identifier,
+              messageId: "loadBeforeRead",
+              data: reference.identifier
+          });
+      });
+      scope.childScopes.forEach(findVariablesInScope);
+    }
+    return {
+      Program() {
+        findVariablesInScope(context.getScope());
+      }
+    }
+  },
+};
+
+
+/*
     return {
       ":matches(VariableDeclarator[init.callee.property.name = 'getSelectedRange'], VariableDeclarator[init.callee.property.name = 'getItem'], VariableDeclarator[init.callee.property.name = 'getRange'])"(
           node: TSESTree.VariableDeclarator
@@ -28,8 +135,7 @@ export = {
         });
       },
     };
-  },
-};
+*/
 
 /*
 Location the load function:
