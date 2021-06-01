c3efc4b721c084b900f0343340254b43728800fe
igor-ribeiiro <igormr98mr@gmail.com>
2021-06-01 09:41:25 -0700

    Code more clean

--- packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
+++ packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
@@ -18,12 +18,9 @@ export = {
   create: function (context: any) {
     return {
       ":matches(VariableDeclarator[init.callee.property.name = 'getSelectedRange'], VariableDeclarator[init.callee.property.name = 'getItem'], VariableDeclarator[init.callee.property.name = 'getRange'])"(
-      //"VariableDeclarator"(
           node: TSESTree.VariableDeclarator
       ) {
         const variableName: string = (node.id as TSESTree.Identifier).name;
-        console.log("Variable name = ");
-        console.log(variableName);
 
         context.report({
           node: node,
@@ -35,14 +32,6 @@ export = {
 };
 
 /*
-Locating Variable:
-VariableDeclarator :matches Identifier.name (this is the variable name) then check for get call expression
-
-Locating the get functions:
-CallExpression[callee.property.name = 'getSelectedRange']
-CallExpression[callee.property.name = 'getItem']
-CallExpression[callee.property.name = 'getRange']
-
 Location the load function:
 CallExpression[callee.property.name='load'][arguments.Literal.value = variableName]
 */
