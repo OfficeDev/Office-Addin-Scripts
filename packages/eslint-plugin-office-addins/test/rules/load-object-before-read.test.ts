9979c96639915371c33858da0a10324a54218691
igor-ribeiiro <igormr98mr@gmail.com>
2021-06-02 13:43:10 -0700

    Better test formatting

--- packages/eslint-plugin-office-addins/test/rules/load-object-before-read.test.ts
+++ packages/eslint-plugin-office-addins/test/rules/load-object-before-read.test.ts
@@ -8,7 +8,8 @@ const ruleTester = new ESLintUtils.RuleTester({
 ruleTester.run('load-object-before-read', rule, {
   valid: [ 
     {
-      code: `var sheetName = 'Sheet1';
+      code: `
+        var sheetName = 'Sheet1';
         var rangeAddress = 'A1:B2';
         var myRange = context.workbook.worksheets.getItem(sheetName).getRange(rangeAddress);  
         myRange.load('address');
@@ -18,19 +19,22 @@ ruleTester.run('load-object-before-read', rule, {
           });`
     },
     {
-      code: `var property = worksheet.getItem("sheet");
+      code: `
+        var property = worksheet.getItem("sheet");
         property.load('G2');
         var variableName = property.G2;`
     },
     {
-      code: `var selectedRange = context.workbook.getSelectedRange();
+      code: `
+        var selectedRange = context.workbook.getSelectedRange();
         selectedRange.load('values');
         if(selectedRange.values === [2]){}`
     }
   ],
   invalid: [
     {
-      code: `var sheetName = 'Sheet1';
+      code: `
+        var sheetName = 'Sheet1';
         var rangeAddress = 'A1:B2';
         var myRange = context.workbook.worksheets.getItem(sheetName).getRange(rangeAddress);  
         myRange.load('address');
@@ -41,22 +45,26 @@ ruleTester.run('load-object-before-read', rule, {
       errors: [{ messageId: "loadBeforeRead" }]
     },
     {
-      code: `var selectedRange = context.workbook.getSelectedRange();
+      code: `
+        var selectedRange = context.workbook.getSelectedRange();
         if(selectedRange.values === ["sampleText"]){}`,
       errors: [{ messageId: "loadBeforeRead" }]
     },
     {
-      code: `var myRange = context.workbook.worksheets.getItem("sheet").getRange("A1");
+      code: `
+        var myRange = context.workbook.worksheets.getItem("sheet").getRange("A1");
         console.log (myRange.adress);`,
       errors: [{ messageId: "loadBeforeRead" }]
     },
     {
-      code: `var selectedRange = context.workbook.getSelectedRange();
+      code: `
+        var selectedRange = context.workbook.getSelectedRange();
         console.log(selectedRange.values);`,
       errors: [{ messageId: "loadBeforeRead" }]
     },
     {
-      code: `var selectedRange = context.workbook.getSelectedRange();
+      code: `
+        var selectedRange = context.workbook.getSelectedRange();
         var test = selectedRange.values;`,
       errors: [{ messageId: "loadBeforeRead" }]
     }
