592bbcc3c1d66a120f355ede623f4f80659afa19
igor-ribeiiro <igormr98mr@gmail.com>
2021-06-02 09:30:41 -0700

    Better formatting in docs

--- packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
+++ packages/eslint-plugin-office-addins/src/rules/load-object-before-read.ts
@@ -71,10 +71,14 @@ export = {
       loadBeforeRead: "An explicit load call needs to be made before reading a proxu object",
     },
     docs: {
-      description: "Before you can read the properties of a proxy object, you must explicitly load the properties",
-      category: <"Best Practices" | "Stylistic Issues" | "Variables" | "Possible Errors">"Possible Errors",
+      description: 
+        "Before you can read the properties of a proxy object, you must explicitly load the properties",
+      category: <
+        "Best Practices" | "Stylistic Issues" | "Variables" | "Possible Errors"
+      >"Possible Errors",
       recommended: <false | "error" | "warn">false,
-      url: "https://docs.microsoft.com/en-us/office/dev/add-ins/develop/application-specific-api-model?view=powerpoint-js-1.1#load",
+      url: 
+        "https://docs.microsoft.com/en-us/office/dev/add-ins/develop/application-specific-api-model?view=powerpoint-js-1.1#load",
     },
     schema: [],
   },
