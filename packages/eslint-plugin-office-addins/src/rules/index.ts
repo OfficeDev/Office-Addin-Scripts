975941cad781c4ea8ee73e467ad408c19d23322c
igor-ribeiiro <igormr98mr@gmail.com>
2021-06-02 09:26:35 -0700

    Fixing rule bug

--- packages/eslint-plugin-office-addins/src/rules/index.ts
+++ packages/eslint-plugin-office-addins/src/rules/index.ts
@@ -1,9 +1,11 @@
 import noOfficeInitialize from "./no-office-initialize";
 import noContextSyncInLoop from "./no-context-sync-in-loop";
 import testForNullUsingIsNullObject from "./test-for-null-using-isNullObject";
+import loadObjectBeforeRead from "./load-object-before-read";
 
 export default {
   "no-context-sync-in-loop": noContextSyncInLoop,
   "no-office-initialize": noOfficeInitialize,
   "test-for-null-using-isNullObject": testForNullUsingIsNullObject,
+  "load-object-before-read": loadObjectBeforeRead,
 };
