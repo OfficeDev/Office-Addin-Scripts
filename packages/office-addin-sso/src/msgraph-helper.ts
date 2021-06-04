24e0a285aee35033c4fc96ee9f07ec4ff01764dd
Darren Miller <darrmill@microsoft.com>
2021-06-04 10:55:18 -0700

    Run linter on office-addin-sso

--- packages/office-addin-sso/src/msgraph-helper.ts
+++ packages/office-addin-sso/src/msgraph-helper.ts
@@ -3,49 +3,58 @@
     This file provides the provides functionality to get Microsoft Graph data.
 */
 
-import { ODataHelper } from './odata-helper';
-import { showMessage } from './message-helper';
-import * as $ from 'jquery';
+import { ODataHelper } from "./odata-helper";
+import { showMessage } from "./message-helper";
+import * as $ from "jquery";
 
-const domain: string = 'graph.microsoft.com';
-const versionURLsegment: string = '/v1.0';
+const domain: string = "graph.microsoft.com";
+const versionURLsegment: string = "/v1.0";
 
-export async function getGraphData(accessToken: string, apiURLsegment: string, queryParamsSegment?: string): Promise<any> {
-    return new Promise<any>(async (resolve, reject) => {
-        try {
-            const oData = await ODataHelper.getData(accessToken, domain, apiURLsegment, versionURLsegment, queryParamsSegment);
-            resolve(oData);
-        } catch (err) {
-            reject(`Error get Graph data. \n${err}`);
-        }
-    });
+export async function getGraphData(
+  accessToken: string,
+  apiURLsegment: string,
+  queryParamsSegment?: string
+): Promise<any> {
+  return new Promise<any>(async (resolve, reject) => {
+    try {
+      const oData = await ODataHelper.getData(
+        accessToken,
+        domain,
+        apiURLsegment,
+        versionURLsegment,
+        queryParamsSegment
+      );
+      resolve(oData);
+    } catch (err) {
+      reject(`Error get Graph data. \n${err}`);
+    }
+  });
 }
 
 export async function getGraphToken(bootstrapToken): Promise<any> {
-    try {
-        let response = await $.ajax({
-            type: "GET",
-            url: "/auth",
-            headers: { Authorization: "Bearer " + bootstrapToken },
-            cache: false
-        });
-        return response;
-    } catch (err) {
-        throw new Error(`Error getting Graph token. \n${err}`);
-    }
-
+  try {
+    let response = await $.ajax({
+      type: "GET",
+      url: "/auth",
+      headers: { Authorization: "Bearer " + bootstrapToken },
+      cache: false,
+    });
+    return response;
+  } catch (err) {
+    throw new Error(`Error getting Graph token. \n${err}`);
+  }
 }
 
 export async function makeGraphApiCall(accessToken: string): Promise<any> {
-    try {
-        const response = await $.ajax({
-            type: "GET",
-            url: "/getuserdata",
-            headers: { access_token: accessToken },
-            cache: false
-        })
-        return response;
-    } catch (err) {
-        showMessage(`Error from Microsoft Graph. \n${err}`);
-    }
+  try {
+    const response = await $.ajax({
+      type: "GET",
+      url: "/getuserdata",
+      headers: { access_token: accessToken },
+      cache: false,
+    });
+    return response;
+  } catch (err) {
+    showMessage(`Error from Microsoft Graph. \n${err}`);
+  }
 }
--- packages/office-addin-sso/src/server.ts
+++ packages/office-addin-sso/src/server.ts
@@ -2,88 +2,97 @@
 /*
     This file provides server configuration, startup and stop
 */
-import * as https from 'https';
-import * as devCerts from 'office-addin-dev-certs';
-import * as manifest from 'office-addin-manifest';
-import { App } from './app';
-import { getSecretFromCredentialStore } from './ssoDataSettings';
-import { usageDataObject } from './defaults';
-require('dotenv').config();
+import * as https from "https";
+import * as devCerts from "office-addin-dev-certs";
+import * as manifest from "office-addin-manifest";
+import { App } from "./app";
+import { getSecretFromCredentialStore } from "./ssoDataSettings";
+import { usageDataObject } from "./defaults";
+
+/* global require, process, console */
+
+require("dotenv").config();
 
 export class SSOService {
-    private app: App;
-    manifestPath: string;
-    private port: number | string;
-    private server: https.Server;
-    private ssoServiceStarted: boolean;
-    constructor(manifestPath: string) {
-        this.port = process.env.PORT || '3000';
-        this.app = new App(this.port);
-        this.app.initialize();
-        this.manifestPath = manifestPath;
-        this.ssoServiceStarted = false;
-    }
+  private app: App;
+  manifestPath: string;
+  private port: number | string;
+  private server: https.Server;
+  private ssoServiceStarted: boolean;
+  constructor(manifestPath: string) {
+    this.port = process.env.PORT || "3000";
+    this.app = new App(this.port);
+    this.app.initialize();
+    this.manifestPath = manifestPath;
+    this.ssoServiceStarted = false;
+  }
 
-    public async startSsoService(isTest: boolean = false): Promise<boolean> {
-        return new Promise<boolean>(async (resolve, reject) => {
-            try {
-                if (isTest) {
-                    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-                }
-                await this.getSecret(isTest);
-                await this.startServer(this.app.appInstance, this.port);
-                this.ssoServiceStarted = true;
-                usageDataObject.reportSuccess('startSsoService()');
-                resolve(true);
-            } catch(err) {
-                usageDataObject.reportException('startSsoService()', err);
-                reject(false);
-            }
-        });
-    }
-
-    private async getSecret(isTest: boolean = false): Promise<void> {
-        const manifestInfo = await manifest.readManifestFile(this.manifestPath);
-        const appSecret = getSecretFromCredentialStore(manifestInfo.displayName, isTest);
-        if (appSecret === '') {
-            const errorMessage: string = 'Call to getSecretFromCredentialStore returned empty string';
-            throw new Error(errorMessage);
+  public async startSsoService(isTest: boolean = false): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      try {
+        if (isTest) {
+          process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
         }
-        process.env.secret = appSecret;
-    }
+        await this.getSecret(isTest);
+        await this.startServer(this.app.appInstance, this.port);
+        this.ssoServiceStarted = true;
+        usageDataObject.reportSuccess("startSsoService()");
+        resolve(true);
+      } catch (err) {
+        usageDataObject.reportException("startSsoService()", err);
+        reject(false);
+      }
+    });
+  }
 
-    public getTestServerState(): boolean {
-        return this.ssoServiceStarted;
+  private async getSecret(isTest: boolean = false): Promise<void> {
+    const manifestInfo = await manifest.readManifestFile(this.manifestPath);
+    const appSecret = getSecretFromCredentialStore(
+      manifestInfo.displayName,
+      isTest
+    );
+    if (appSecret === "") {
+      const errorMessage: string =
+        "Call to getSecretFromCredentialStore returned empty string";
+      throw new Error(errorMessage);
     }
+    process.env.secret = appSecret;
+  }
 
-    public async startServer(app, port): Promise<boolean> {
-        return new Promise<boolean>(async (resolve, reject) => {
-            try {
-                const options = await devCerts.getHttpsServerOptions();
-                this.server = https.createServer(options, app).listen(port, () => console.log(`Server running on ${port}`));
-                resolve(true);
-            } catch (err) {
-                const errorMessage: string = `Unable to start test server on port ${port}: \n${err}`;
-                reject(errorMessage);
-            }
-        });
-    }
+  public getTestServerState(): boolean {
+    return this.ssoServiceStarted;
+  }
 
-    public async stopServer(): Promise<boolean> {
-        return new Promise<boolean>(async (resolve, reject) => {
-            if (this.ssoServiceStarted) {
-                try {
-                    this.server.close();
-                    this.ssoServiceStarted = false;
-                    resolve(true);
-                } catch (err) {
-                    const errorMessage: string = `Unable to stop test server: \n${err}`;
-                    reject(new Error(errorMessage));
-                }
-            } else {
-                // test server not started
-                resolve(false);
-            }
-        });
-    }
+  public async startServer(app, port): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      try {
+        const options = await devCerts.getHttpsServerOptions();
+        this.server = https
+          .createServer(options, app)
+          .listen(port, () => console.log(`Server running on ${port}`));
+        resolve(true);
+      } catch (err) {
+        const errorMessage: string = `Unable to start test server on port ${port}: \n${err}`;
+        reject(errorMessage);
+      }
+    });
+  }
+
+  public async stopServer(): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      if (this.ssoServiceStarted) {
+        try {
+          this.server.close();
+          this.ssoServiceStarted = false;
+          resolve(true);
+        } catch (err) {
+          const errorMessage: string = `Unable to stop test server: \n${err}`;
+          reject(new Error(errorMessage));
+        }
+      } else {
+        // test server not started
+        resolve(false);
+      }
+    });
+  }
 }
