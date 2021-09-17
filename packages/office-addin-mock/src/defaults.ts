c5a98c2d9f7ea6f3f6d414bd94dd37a649271b33
igor-ribeiiro <igormr98mr@gmail.com>
2021-09-17 14:54:37 -0700

    Renaming package

---
+++ packages/office-addin-mock/src/defaults.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as usageData from "office-addin-usage-data";
+
+// Usage data defaults
+export const usageDataObject: usageData.OfficeAddinUsageData =
+  new usageData.OfficeAddinUsageData({
+    projectName: "office-addin-unit-test",
+    instrumentationKey: usageData.instrumentationKeyForOfficeAddinCLITools,
+    raisePrompt: false,
+  });
