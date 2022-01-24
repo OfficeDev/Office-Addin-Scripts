aaf670a7bedf65ae6ef90722fb7eb037ffd10a4f
igor-ribeiiro <igormr98mr@gmail.com>
2022-01-24 14:30:50 -0800

    Rmoving unnecessary commander parameter

--- packages/office-addin-cli/src/commands.ts
+++ packages/office-addin-cli/src/commands.ts
@@ -1,18 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as commander from "commander";
 import { logErrorMessage } from "./log";
 import { usageDataObject } from "./defaults";
 
-/* global console, process */
-
-export async function upgrade(
-  manifestPath: string,
-  command: commander.Command /* eslint-disable-line @typescript-eslint/no-unused-vars */
-) {
+export async function upgrade(manifestPath: string) {
   try {
+    // Convert manifest .xml to manifest.json
+    // Do any more needed modifications on the code?
+
     usageDataObject.reportSuccess("upgrade");
+    throw new Error("Upgrade function is not ready yet.");
   } catch (err: any) {
     usageDataObject.reportException("upgrade", err);
     logErrorMessage(err);
