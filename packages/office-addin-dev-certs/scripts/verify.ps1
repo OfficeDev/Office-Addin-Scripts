f50b941eafbb995753200444e089dcf64510936c
Darren Miller <darrmill@microsoft.com>
2022-02-03 10:54:42 -0800

    Adjust the date used to check cert validity

--- packages/office-addin-dev-certs/scripts/verify.ps1
+++ packages/office-addin-dev-certs/scripts/verify.ps1
@@ -4,9 +4,9 @@ if($args.Count -ne 1){
 
 $caCertificateName=$args[0]
 if(Get-Command -name Import-Certificate -ErrorAction SilentlyContinue){
-    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object Issuer -like "*CN=$caCertificateName*" | Where-Object { $_.NotAfter -gt [datetime]::today } | Format-List
+    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object Issuer -like "*CN=$caCertificateName*" | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) } | Format-List
 }
 else{
     # Legacy system support
-    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object { $_.Subject -like "*CN=$caCertificateName*"} | Where-Object { $_.NotAfter -gt [datetime]::today } | Format-List
+    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object { $_.Subject -like "*CN=$caCertificateName*"} | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) } | Format-List
 }
