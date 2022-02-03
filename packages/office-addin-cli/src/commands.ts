85fd90adcce04ce8c0db18fc209375297aa6a928
igor-ribeiiro <igormr98mr@gmail.com>
2022-02-03 13:36:15 -0800

    import logError from the right place

--- packages/office-addin-cli/src/commands.ts
+++ packages/office-addin-cli/src/commands.ts
@@ -1,14 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { logErrorMessage } from "./log";
+import { logErrorMessage } from "office-addin-usage-data";
 import { usageDataObject } from "./defaults";
 
 export async function convert(manifestPath: string) {
   try {
     // Convert manifest .xml to manifest.json
     // Do any more needed modifications on the code?
-
     usageDataObject.reportSuccess("convert");
     throw new Error("Upgrade function is not ready yet.");
   } catch (err: any) {
