b9499cc021294d85fb189442fa3f54015b89a668
igor-ribeiiro <igormr98mr@gmail.com>
2022-04-07 14:34:08 -0700

    Creating create zip function

--- packages/office-addin-project/src/convert.ts
+++ packages/office-addin-project/src/convert.ts
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import * as  archiver from "archiver";
 import * as fs from "fs";
 import * as inquirer from "inquirer";
+import * as path from "path";
 import * as util from "util";
 import { exec } from "child_process";
 import { ExpectedError } from "office-addin-usage-data";
@@ -10,24 +12,25 @@ import { ExpectedError } from "office-addin-usage-data";
 /* global console */
 
 export async function convertProject(manifestPath: string = "./manifest.xml") {
-  if (manifestPath.endsWith(".json")) {
-    throw new ExpectedError(
-      `The convert command only works on xml manifest based projects`
-    );
-  }
+  // if (manifestPath.endsWith(".json")) {
+  //   throw new ExpectedError(
+  //     `The convert command only works on xml manifest based projects`
+  //   );
+  // }
 
-  if (!fs.existsSync(manifestPath)) {
-    throw new ExpectedError(
-      `The manifest file '${manifestPath}' does not exist`
-    );
-  }
+  // if (!fs.existsSync(manifestPath)) {
+  //   throw new ExpectedError(
+  //     `The manifest file '${manifestPath}' does not exist`
+  //   );
+  // }
 
-  const shouldContinue = await asksForUserConfirmation();
-  if (!shouldContinue) {
-    return;
-  }
-  updatePackages();
-  await updateManifestXmlReferences();
+  // const shouldContinue = await asksForUserConfirmation();
+  // if (!shouldContinue) {
+  //   return;
+  // }
+  await backupProject();
+  // updatePackages();
+  // await updateManifestXmlReferences();
 }
 
 async function asksForUserConfirmation(): Promise<boolean> {
@@ -38,6 +41,27 @@ async function asksForUserConfirmation(): Promise<boolean> {
   };
   const answers = await inquirer.prompt([question]);
   return (answers as any).didUserConfirm;
+}
+
+async function backupProject() {
+  const outputPath = `backup.zip`;
+  const stream = fs.createWriteStream(outputPath);
+  const archive = archiver('zip', { zlib: { level: 9 }});
+  const sourceDir = path.resolve(__dirname);
+
+  return new Promise<void>((resolve, reject) => {
+    archive
+      .directory(sourceDir, false)
+      .on('error', err => reject(err))
+      .pipe(stream)
+    ;
+
+    stream.on('close', () => {
+      console.log(`A backup of your project was create to ${path.resolve(outputPath)}`);
+      resolve();
+    });
+    archive.finalize();
+  });
 }
 
 function updatePackages(): void {
