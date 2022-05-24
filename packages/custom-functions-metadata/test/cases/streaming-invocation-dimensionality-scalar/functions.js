d155cd86944fe6b7cfa4f4fdf869ae5b9f157448
Darren Miller <darrmill@microsoft.com>
2022-05-23 18:58:18 -0700

    Add tests for generic array types

---
+++ packages/custom-functions-metadata/test/cases/streaming-invocation-dimensionality-scalar/functions.js
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+/**
+ * Test function for streaming type
+ * @param x {string}
+ * @param invocation {CustomFunctions.StreamingInvocation<string[]>}
+ * @customfunction
+ */
+function streamingTest(x, invocation) {
+    // Empty
+}
