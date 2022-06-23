033c759b4bfc78923daca75e48bb696bf6ea24f8
yilin4 <yilin4@microsoft.com>
2022-06-23 17:30:01 +0800

    test case

---
+++ packages/custom-functions-metadata/test/cases/field/functions.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+/**
+ * basic custom function
+ * @customfunction
+ */
+ function basic() {
+    // empty
+}
+
+/**
+ * @field allowCustomDataForDataTypeAny true
+ */
