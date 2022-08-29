f616cac0981fb9fbeb3cb60d095fcbc1a91bccb9
igor-ribeiiro <igormr98mr@gmail.com>
2022-08-29 14:28:39 -0700

    Fixing  await error

--- packages/office-addin-project/test/test.ts
+++ packages/office-addin-project/test/test.ts
@@ -44,7 +44,7 @@ describe("office-addin-project tests", function() {
         this.timeout(6000);
         const manifestPath = "./test/invalid.manifest.xml";
         const outputPath = "./out";
-        await convert(manifestPath, outputPath);
+        convert(manifestPath, outputPath);
         assert.strictEqual(fs.existsSync(path.join(outputPath, "manifest.json")), false);
       });
     });
