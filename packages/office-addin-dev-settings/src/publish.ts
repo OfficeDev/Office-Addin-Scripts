f7c844d1b79979e8d77b46224f55880cddd359a7
Darren Miller <darrmill@microsoft.com>
2022-08-31 14:56:54 -0700

    specifiy version of teamsfx cli package

--- packages/office-addin-dev-settings/src/publish.ts
+++ packages/office-addin-dev-settings/src/publish.ts
@@ -10,7 +10,7 @@ import * as path from "path";
 export async function registerWithTeams(zipPath: string): Promise<string> {
   return new Promise((resolve, reject) => {
     if (zipPath.endsWith(".zip") && fs.existsSync(zipPath)) {
-      const sideloadCommand = `npx @microsoft/teamsfx-cli m365 sideloading --file-path ${zipPath}`;
+      const sideloadCommand = `npx @microsoft/teamsfx-cli@1.0.6-alpha.18494658a.0 m365 sideloading --file-path ${zipPath}`;
 
       console.log(`running: ${sideloadCommand}`);
       childProcess.exec(sideloadCommand, (error, stdout, stderr) => {
@@ -32,7 +32,7 @@ export async function registerWithTeams(zipPath: string): Promise<string> {
 
 export async function updateM365Account(operation: "login" | "logout"): Promise<void> {
   return new Promise((resolve, reject) => {
-    const loginCommand = `npx @microsoft/teamsfx-cli account ${operation} m365`;
+    const loginCommand = `npx @microsoft/teamsfx-cli@1.0.6-alpha.18494658a.0 account ${operation} m365`;
 
     console.log(`running: ${loginCommand}`);
     childProcess.exec(loginCommand, (error, stdout, stderr) => {
@@ -49,7 +49,7 @@ export async function updateM365Account(operation: "login" | "logout"): Promise<
 
 export async function unacquireWithTeams(titleId: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    const unacquireCommand = `npx @microsoft/teamsfx-cli m365 unacquire --title-id ${titleId}`;
+    const unacquireCommand = `npx @microsoft/teamsfx-cli@1.0.6-alpha.18494658a.0 m365 unacquire --title-id ${titleId}`;
 
     console.log(`running: ${unacquireCommand}`);
     childProcess.exec(unacquireCommand, (error, stdout, stderr) => {
