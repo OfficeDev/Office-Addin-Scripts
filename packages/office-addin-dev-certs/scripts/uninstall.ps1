52a1fba87540daa4792fb79f423a0e53387e286b
Cédric Bouzyd <cedricbouzyd@microsoft.com>
2022-12-09 17:59:10 +0100

    Initial commit

--- packages/office-addin-dev-certs/scripts/uninstall.ps1
+++ packages/office-addin-dev-certs/scripts/uninstall.ps1
@@ -2,9 +2,17 @@ if($args.Count -ne 2){
     throw "Usage: uninstall.ps1 <LocalMachine | CurrentUser> <CA-certficate-name>"
 }
 
+# Without this, the script always succeeds (exit code = 0)
+$ErrorActionPreference = 'Stop'
+
 $machine = $args[0]
 $caCertificateName=$args[1]
 if(Get-Command -name Import-Certificate -ErrorAction SilentlyContinue){
+    if ($PSVersionTable.PSVersion.Major -le 5) {
+        # The following line is required in case pwsh is one of the parent callers
+        # because the changes it makes to PSModulePath are not backward compatible with Windows powershell.
+        $env:PSModulePath = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
+    }
     Get-ChildItem  cert:\\$machine\\Root | Where-Object { $_.IssuerName.Name -like "*CN=$caCertificateName*" } |  Remove-Item
 }
 else{
--- packages/office-addin-dev-certs/scripts/verify.ps1
+++ packages/office-addin-dev-certs/scripts/verify.ps1
@@ -1,12 +1,63 @@
-if($args.Count -ne 1){
-    throw "Usage: verify.ps1 <CA-certficate-name>"
+Param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $CaCertificateName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $CaCertificatePath,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $LocalhostCertificatePath,
+
+    [switch]
+    $ReturnInvalidCertificate
+)
+
+# Without this, the script always succeeds (exit code = 0)
+$ErrorActionPreference = 'Stop'
+
+if ($PSVersionTable.PSVersion.Major -le 5) {
+    # The following line is required in case pwsh is one of the parent callers
+    # because the changes it makes to PSModulePath are not backward compatible with Windows powershell.
+    $env:PSModulePath = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
 }
 
-$caCertificateName=$args[0]
 if(Get-Command -name Import-Certificate -ErrorAction SilentlyContinue){
-    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object Issuer -like "*CN=$caCertificateName*" | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) } | Format-List
+    $result = Get-ChildItem cert:\\CurrentUser\\Root | Where-Object Issuer -like "*CN=$CaCertificateName*"
+    if (!$ReturnInvalidCertificate) {
+        $result = $result | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) }
+        if ($result -and ($result.Length -eq 1) -and (Test-Path $CaCertificatePath) -and (Test-Path $LocalhostCertificatePath)) {
+            # Check that CA certificate in store is the same as ca.crt
+            $caCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($CaCertificatePath)
+            $caThumbprint = $caCert.Thumbprint
+
+            $result = $result | Where-Object Thumbprint -eq $caThumbprint
+
+            if ($result) {
+                # Check that it matches the issuer of localhost.crt
+                $localhostCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($LocalhostCertificatePath)
+
+                $localhostChain = [System.Security.Cryptography.X509Certificates.X509Chain]::new()
+                $localhostChain.Build($localhostCert) | Out-Null
+                $localhostTrustedIssuer = $localhostChain.ChainElements.Certificate | Where-Object { $_.Subject -eq $localhostCert.Issuer -and $_.Thumbprint -eq $caThumbprint }
+                if (!$localhostTrustedIssuer) {
+                    $result = $null
+                }
+            }
+        }
+        else {
+            $result = $null
+        }
+    }
+
+    $result | Format-List
 }
 else{
     # Legacy system support
-    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object { $_.Subject -like "*CN=$caCertificateName*"} | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) } | Format-List
+    Get-ChildItem cert:\\CurrentUser\\Root | Where-Object { $_.Subject -like "*CN=$CaCertificateName*"} | Where-Object { $_.NotAfter -gt [datetime]::today.AddDays(-1) } | Format-List
 }
