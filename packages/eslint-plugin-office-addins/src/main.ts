093b7284e1b4e075bd2077b579accb79f87e474a
Victoria Iannotti <viiannotti@microsoft.com>
2025-04-16 15:03:33 -0700

    Upgrade Lerna and fix related issues

--- packages/eslint-plugin-office-addins/src/main.ts
+++ packages/eslint-plugin-office-addins/src/main.ts
@@ -2,9 +2,12 @@ import rules from "./rules";
 import tsParser from "@typescript-eslint/parser";
 import typescriptplugin from "@typescript-eslint/eslint-plugin";
 import prettierplugin from "eslint-plugin-prettier";
-import reactplugin from "eslint-plugin-react";
+//import reactplugin from "eslint-plugin-react";
 import eslintjs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const reactplugin = require("eslint-plugin-react");
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const reactnativeplugin = require("eslint-plugin-react-native");
