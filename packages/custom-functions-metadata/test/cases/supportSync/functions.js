936b0690eb5fd1640ffef41a50375b6702cccc20
Herman Wen <hermanwen@microsoft.com>
2025-09-01 10:51:15 +0800

    Add success test case

---
+++ packages/custom-functions-metadata/test/cases/supportSync/functions.js
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+/**
+ * Test function for supportSync tag
+ * @param x {string}
+ * @customfunction
+ * @supportSync
+ */
+function supportSyncTest(x) {
+    // Empty
+}
