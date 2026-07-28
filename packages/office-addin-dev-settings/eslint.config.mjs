9746fab25b041befff903f27cad0d240e4e2be05
Darren Miller <darrmill@microsoft.com>
2026-07-28 13:37:52 -0700

    Remove @microsoft/eslint-plugin-sdl

--- packages/office-addin-dev-settings/eslint.config.mjs
+++ packages/office-addin-dev-settings/eslint.config.mjs
@@ -1,20 +1,14 @@
 import officeAddins from "eslint-plugin-office-addins";
 import tsParser from "@typescript-eslint/parser";
-import sdl from "@microsoft/eslint-plugin-sdl";
 
 export default [
   ...officeAddins.configs.recommended,
-  ...sdl.configs.recommended,
   {
     plugins: {
       "office-addins": officeAddins,
     },
     languageOptions: {
       parser: tsParser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: "__dirname"
-      }
     },
   },
 ];
