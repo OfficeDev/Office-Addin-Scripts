f739b979a14ce4c56b56d76699e25436d3f7eeab
Darren Miller <darrmill@microsoft.com>
2026-07-29 16:41:21 -0700

    Address review comments

--- packages/office-addin-node-debugger/eslint.config.mjs
+++ packages/office-addin-node-debugger/eslint.config.mjs
@@ -11,7 +11,7 @@ export default [
       parser: tsParser,
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: "__dirname",
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
